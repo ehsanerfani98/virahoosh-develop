d906a5a934715965ba4dd79af3469b5b178fc183
Realesi89 <ehsan.bavaghar1989@gmail.com>
2025-07-21 20:46:03 +0330

    fix config

--- core/config.py
+++ core/config.py
@@ -1,12 +1,25 @@
 # core/config.py
-import pytz;
+import pytz
 import os
+import urllib.parse
 from dotenv import load_dotenv
+
 print("Current working directory:", os.getcwd())
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DB_CONNECTION") + "+pymysql://"+ os.getenv("DB_USERNAME") +":"+ os.getenv("DB_PASSWORD") +"@"+ os.getenv("DB_HOST") +":"+ os.getenv("DB_PORT") +"/"+ os.getenv("DB_DATABASE") +""
+db_user = urllib.parse.quote_plus(os.getenv("DB_USERNAME"))
+db_pass = urllib.parse.quote_plus(os.getenv("DB_PASSWORD"))
+
+DATABASE_URL = (
+    os.getenv("DB_CONNECTION") + "+pymysql://"
+    + db_user + ":"
+    + db_pass + "@"
+    + os.getenv("DB_HOST") + ":"
+    + os.getenv("DB_PORT") + "/"
+    + os.getenv("DB_DATABASE")
+)
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 AUTH_HEADER = os.getenv("AUTH_HEADER")
