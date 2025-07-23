c618f3b2c4f21f7fe903c5468668231cf76e5cee
webcomprojects <webcom.projects@gmail.com>
2025-07-23 10:12:43 +0330

    add uuid to assistant tables

--- routers/admin.py
+++ routers/admin.py
@@ -1033,6 +1033,7 @@ async def create_assistant(
         faiss_url=None,
         pkl_url=None,
         excel_url=saved_file_path,
+        slug=str(uuid.uuid4())
     )
     db.add(assistant)
     db.commit()
